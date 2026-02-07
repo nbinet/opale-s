f25038ee384cb7ff50fb3bb4a9984e3964f9f61b
nbinet <nathan.binet99@gmail.com>
2026-02-07 13:30:58 +0100

    add Instagram logo to Footer component and enhance layout for better visibility

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="bg-[#F1E8DD] text-[#D29A33] flex flex-col items-center justify-evenly gap-4 p-4 -mx-4 -mb-4">
-    <div>
-      <p>06 73 65 98 76</p>
-      <p>associationsiliven@gmail.com</p>
-    </div>
-    <div>
-      <img src="/public/instagram.png" alt="Logo Instagram" class="h-14" />
-      <p>opaleis_events</p>
+    <p class="text-center great-vibes-regular sm:text-2xl md:text-4xl">“L’art de sublimer le corps sous toutes ses formes”</p>
+    <img src="/public/logo.png" alt="Logo Opaleïs" class="h-14 md:h-24 -mb-4" />
+    <div class="flex items-end justify-between w-full relative">
+      <div class="flex flex-col gap-2">
+        <p>06 73 65 98 76</p>
+        <p>associationsiliven@gmail.com</p>
+      </div>
+      <div class="flex flex-col items-center">
+        <img src="/public/instagram.png" alt="Instagram" class="h-14" />
+        <p>opaleis_events</p>
+      </div>
     </div>
   </div>
 </template>
