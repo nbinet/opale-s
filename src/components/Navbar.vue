51a25565c2f2ebdc89c147197ac64ebfcfa3c463
nbinet <nathan.binet99@gmail.com>
2026-03-05 19:38:23 +0100

    fix: adjust spacing in Navbar component for improved layout consistency

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,8 +5,8 @@ import logo from '@/assets/images/logo.png'
 <template>
   <nav class="text-[#D29A33] uppercase text-sm flex items-center justify-between gap-4 py-4 md:px-20 overflow-visible h-14">
     <router-link to="/"><img :src="logo" alt="Logo Opaleïs" class="h-14" /></router-link>
-    <div class="flex items-center gap-20">
-      <router-link to="/">Accueil</router-link>
+    <div class="flex items-center gap-10 md:gap-20">
+      <router-link to="/" class="hidden sm:block">Accueil</router-link>
       <router-link to="/pro">Espace pro</router-link>
       <router-link to="/contact">Contact</router-link>
     </div>
