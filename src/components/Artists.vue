0906ac06e33603d90d46e9f91517a545553dab7e
nbinet <nathan.binet99@gmail.com>
2026-04-16 19:33:39 +0200

    feat: add new partner images to Artists component and update the artist list

--- src/components/Artists.vue
+++ src/components/Artists.vue
@@ -1,25 +1,37 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import nails from '@/assets/images/partners/nails_addict_caen.png'
 import mains from '@/assets/images/partners/lesmainsetoilees.png'
 import cocongle from '@/assets/images/partners/cocongle__.jpg'
+import carrecourt from '@/assets/images/partners/carrecourt_.png'
+import lucie from '@/assets/images/partners/lucie_ausina.png'
+import nailstudio from '@/assets/images/partners/nailstudiobysab.png'
+import sabrina from '@/assets/images/partners/sabrina.beautystudio.caen.png'
+import sheena from '@/assets/images/partners/sheenajmanucure.png'
 import haspa from '@/assets/images/partners/haspa_ttt.png'
 import pegasus from '@/assets/images/partners/pegasus_ink14.png'
 import karmeo from '@/assets/images/partners/_karmeo.tatt.png'
 import inkmagination from '@/assets/images/partners/inkmaginationtattoo.png'
 import kuuro from '@/assets/images/partners/kuuro.tattoo.png'
 import ossuary from '@/assets/images/partners/ossuary.tattoo.png'
+import palette from '@/assets/images/partners/palette.jpg'
 
 const artistes = ref([
     { image: nails, insta: 'nails_addict_caen_', category: 'Prothésiste ongulaire', link: 'https://www.instagram.com/nails_addict_caen_/' },
     { image: mains, insta: 'lesmainsetoilees', category: 'Prothésiste ongulaire', link: 'https://www.instagram.com/lesmainsetoilees/' },
     { image: cocongle, insta: 'cocongle__', category: 'Prothésiste ongulaire', link: 'https://www.instagram.com/cocongle__/' },
+    { image: carrecourt, insta: 'carrecourt_', category: 'Prothésiste ongulaire', link: 'https://www.instagram.com/carrecourt_/' },
+    { image: lucie, insta: 'lucie_ausina', category: 'Prothésiste ongulaire', link: 'https://www.instagram.com/lucie_ausina/' },
+    { image: nailstudio, insta: 'nailstudiobysab', category: 'Prothésiste ongulaire', link: 'https://www.instagram.com/nailstudiobysab/' },
+    { image: sabrina, insta: 'sabrina.beautystudio.caen', category: 'Prothésiste ongulaire', link: 'https://www.instagram.com/sabrina.beautystudio.caen/' },
+    { image: sheena, insta: 'sheenajmanucure', category: 'Prothésiste ongulaire', link: 'https://www.instagram.com/sheenajmanucure/' },
     { image: haspa, insta: 'haspa_ttt', category: 'Tattoo artist', link: 'https://www.instagram.com/haspa_ttt/' },
     { image: pegasus, insta: 'pegasus_ink14', category: 'Tattoo artist', link: 'https://www.instagram.com/pegasus_ink14/' },
     { image: karmeo, insta: '_karmeo.tatt', category: 'Tattoo artist', link: 'https://www.instagram.com/_karmeo.tatt/' },
     { image: inkmagination, insta: 'inkmaginationtattoo', category: 'Tattoo artist', link: 'https://www.instagram.com/inkmaginationtattoo/' },
     { image: kuuro, insta: 'kuuro.tattoo', category: 'Tattoo artist', link: 'https://www.instagram.com/kuuro.tattoo/' },
     { image: ossuary, insta: 'ossuary.tattoo', category: 'Tattoo artist', link: 'https://www.instagram.com/ossuary.tattoo/' },
+    { image: palette, insta: 'palette_sucree_patisserie', category: 'Pâtisserie', link: 'https://www.instagram.com/palette_sucree_patisserie/' }
 
 ])
 
